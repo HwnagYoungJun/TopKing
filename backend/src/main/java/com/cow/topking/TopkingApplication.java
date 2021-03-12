50b7e210f0ae3dadcc73cc8dceafe008f658af25
HwnagYoungJun <dudghkd4512@gmail.com>
2021-03-12 16:57:17 +0900

    init

---
+++ backend/src/main/java/com/cow/topking/TopkingApplication.java
@@ -0,0 +1,13 @@
+package com.cow.topking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopkingApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/cow/topking/TopkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cow.topking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
